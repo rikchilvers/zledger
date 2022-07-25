a46c8dd3eaeb3d3949b5175ba7462556351f70b5
Rik Chilvers <rikchilvers@fastmail.com>
2022-07-25 17:23:09 +0100

    SPOT + remove indian numbering check

--- src/decimal.zig
+++ src/decimal.zig
@@ -1,6 +1,9 @@
 const std = @import("std");
 
-const Error = error{UnexpectedCharacter};
+const Error = error{
+    UnexpectedCharacter,
+    IncorrectThousandMark,
+};
 const MaxCharacters = 64;
 
 const Self = @This();
@@ -16,9 +19,8 @@ source: []u8,
 ownedSource: bool,
 
 pub const RenderingInformation = struct {
-    groupSeparator: u8 = 0, // e.g. 24,000
+    thousandMark: u8 = 0, // e.g. 24,000
     decimalSeparator: u8 = 0, // e.g. 3.141
-    indianNumbering: bool = false, // e.g. 10,00,000
 };
 
 // This function does 3 different jobs.
@@ -37,13 +39,12 @@ pub fn initAndFormat(allocator: std.mem.Allocator, source: []u8, renderingInform
     decimal.ownedSource = false;
 
     // Temporary storage of rendering information.
-    var groupSeparator: u8 = 0;
+    var thousandMark: u8 = 0;
     var decimalSeparator: u8 = 0;
-    var indianNumbering = false;
 
     // Index into the source
     var i: usize = 0;
-    // Position of last comma (used to guess if the source is formatted using Indian notation)
+    // Position of last thousand-mark
     var j: usize = 0;
 
     // We use two 32 byte arrays to temporarily hold digits either side of the decimal separator.
@@ -86,29 +87,34 @@ pub fn initAndFormat(allocator: std.mem.Allocator, source: []u8, renderingInform
             ',' => {
                 switch (decimalSeparator) {
                     0 => {
-                        // treat the first , as a decimal separator
+                        // treat the first comma as a decimal separator
                         decimalSeparator = c;
                         decimal.fractional = decimal.digits + 1;
                     },
                     '.' => {
-                        // afterwards, treat it as a group separator
-                        groupSeparator = ',';
+                        // if we've seen a period, we shouldn't be seeing commas
+                        return Error.UnexpectedCharacter;
                     },
                     else => {},
                 }
 
-                if (i - j == 3) {
-                    // speculative at this point
-                    indianNumbering = true;
-                }
+                if (thousandMark == '_') return Error.UnexpectedCharacter;
+
+                if (j > 0 and i - j != 4) return Error.IncorrectThousandMark;
+
                 j = i;
             },
             '_' => {
-                groupSeparator = '_';
+                if (thousandMark == ',') return Error.UnexpectedCharacter;
+                if (j > 0 and i - j != 4) return Error.IncorrectThousandMark;
+                thousandMark = '_';
+                j = i;
             },
             '.' => {
+                if (j > 0 and i - j != 4) return Error.IncorrectThousandMark;
+
                 if (decimalSeparator == ',') {
-                    groupSeparator = ',';
+                    thousandMark = ',';
                     // Because we previously treated a comma as the decimal separator,
                     // all the fraction digits should now be moved to the integer digits
                     if (iF > 0) {
@@ -121,12 +127,6 @@ pub fn initAndFormat(allocator: std.mem.Allocator, source: []u8, renderingInform
                 decimalSeparator = '.';
 
                 decimal.fractional = decimal.digits + 1;
-
-                // If we've detected the number might be formated to match indian numbering,
-                // the previous , should be 3 digits prior to the decimal point
-                if (indianNumbering) {
-                    indianNumbering = i - j == 4;
-                }
             },
             else => {
                 return Error.UnexpectedCharacter;
@@ -141,9 +141,8 @@ pub fn initAndFormat(allocator: std.mem.Allocator, source: []u8, renderingInform
     }
 
     if (renderingInformation) |ri| {
-        ri.*.groupSeparator = groupSeparator;
+        ri.*.thousandMark = thousandMark;
         ri.*.decimalSeparator = decimalSeparator;
-        ri.*.indianNumbering = indianNumbering;
     }
 
     // Replace the source with the formatted version.
@@ -162,115 +161,12 @@ pub fn initAndFormat(allocator: std.mem.Allocator, source: []u8, renderingInform
     return decimal;
 }
 
-pub fn initWithSource(allocator: std.mem.Allocator, source: []u8, renderingInformation: ?*RenderingInformation) !*Self {
-    var decimal = allocator.create(Self) catch unreachable;
-    errdefer allocator.destroy(decimal);
-
-    decimal.positive = true;
-    decimal.fractional = 0;
-    decimal.digits = 0;
-    decimal.source = source;
-    decimal.ownedSource = false;
-
-    var groupSeparator: u8 = 0;
-    var decimalSeparator: u8 = 0;
-    var indianNumbering = false;
-
-    // used to track index into the source
-    var i: usize = 0;
-    // used to track position of , in the source
-    var j: usize = 0;
-
-    // Skip over spaces at the start of the source
-    while (true) : (i += 1) {
-        if (source[i] != ' ') break;
-    }
-
-    // Find the sign
-    if (source[i] == '-') {
-        decimal.positive = false;
-        i += 1;
-    } else if (source[i] == '+') {
-        i += 1;
-    }
-
-    // skip over leading zeroes
-    while (i < std.mem.len(source)) : (i += 1) {
-        if (source[i] != '0') break;
-    }
-
-    while (i < std.mem.len(source)) : (i += 1) {
-        const c = source[i];
-
-        switch (c) {
-            '0'...'9' => {
-                decimal.digits += 1;
-            },
-            ',' => {
-                switch (decimalSeparator) {
-                    0 => {
-                        // treat the first , as a decimal separator
-                        decimalSeparator = c;
-                        decimal.fractional = decimal.digits + 1;
-                    },
-                    '.' => {
-                        // afterwards, treat it as a group separator
-                        groupSeparator = ',';
-                    },
-                    else => {},
-                }
-
-                if (i - j == 3) {
-                    // speculative at this point
-                    indianNumbering = true;
-                }
-                j = i;
-            },
-            '_' => {
-                groupSeparator = '_';
-            },
-            '.' => {
-                if (decimalSeparator == ',') {
-                    groupSeparator = ',';
-                }
-                decimalSeparator = '.';
-
-                decimal.fractional = decimal.digits + 1;
-
-                // If we've detected the number might be formated to match indian numbering,
-                // the previous , should be 3 digits prior to the decimal point
-                if (indianNumbering) {
-                    indianNumbering = i - j == 4;
-                }
-            },
-            else => {
-                std.log.info("got a {}", .{c});
-                return Error.UnexpectedCharacter;
-            },
-        }
-    }
-
-    // Up to now, decimal.fractional has been storing the index of the first digit after the decimalSeparator
-    // We need to change this now to reflect the actual number of fractional digits
-    if (decimal.fractional > 0) {
-        decimal.fractional = decimal.digits - (decimal.fractional - 1);
-    }
-
-    if (renderingInformation) |ri| {
-        ri.*.groupSeparator = groupSeparator;
-        ri.*.decimalSeparator = decimalSeparator;
-        ri.*.indianNumbering = indianNumbering;
-    }
-
-    return decimal;
-}
-
 /// Initialises the Decimal along with space for its source
 pub fn init(allocator: std.mem.Allocator, number: []const u8, separator: ?*RenderingInformation) !*Self {
     var source = allocator.allocSentinel(u8, std.mem.len(number), 0) catch unreachable;
     std.mem.copy(u8, source, number);
     errdefer allocator.free(source);
-    var result = try initWithSource(allocator, source, separator);
+    var result = try initAndFormat(allocator, source, separator);
     result.ownedSource = true;
     return result;
 }
@@ -386,16 +282,6 @@ test "init allocates for source" {
     try std.testing.expectEqual(@as(u32, 5), d.fractional);
 }
 
-test "ignores space at the start" {
-    // std.testing.log_level = .debug;
-
-    const d = try Self.init(std.testing.allocator, "   314159", null);
-    defer d.deinit(std.testing.allocator);
-
-    try std.testing.expectEqual(@as(u32, 6), d.digits);
-    try std.testing.expectEqual(@as(u32, 0), d.fractional);
-}
-
 test "parses integers" {
     // std.testing.log_level = .debug;
 
@@ -428,15 +314,14 @@ test "parses negative integers" {
     try std.testing.expect(!d.positive);
 }
 
-test "parsing returns error on unexpected character" {
+test "error on unexpected character" {
     // std.testing.log_level = .debug;
 
     const d = Self.init(std.testing.allocator, "314159a", null);
-
     try std.testing.expectError(Error.UnexpectedCharacter, d);
 }
 
-test "parses decimals with . as decimal separator" {
+test "parses decimals with period as decimal separator" {
     // std.testing.log_level = .debug;
 
     const d = try Self.init(std.testing.allocator, "3.14159", null);
@@ -446,7 +331,7 @@ test "parses decimals with . as decimal separator" {
     try std.testing.expectEqual(@as(u32, 5), d.fractional);
 }
 
-test "parses decimals with , as decimal separator" {
+test "parses decimals with comma as decimal separator" {
     // std.testing.log_level = .debug;
 
     const d = try Self.init(std.testing.allocator, "3,14159", null);
@@ -466,54 +351,31 @@ test "parses decimals with thousand separators" {
     try std.testing.expectEqual(@as(u32, 9), d.digits);
     try std.testing.expectEqual(@as(u32, 2), d.fractional);
 
-    try std.testing.expectEqual(@as(u8, ','), ri.groupSeparator);
+    try std.testing.expectEqual(@as(u8, ','), ri.thousandMark);
     try std.testing.expectEqual(@as(u8, '.'), ri.decimalSeparator);
-    try std.testing.expectEqual(false, ri.indianNumbering);
 }
 
-test "parses decimals with thousand separators after decimal point" {
-    // std.testing.log_level = .debug;
-
-    var ri = RenderingInformation{};
-    const d = try Self.init(std.testing.allocator, "3_141_592.650_123_430", &ri);
-    defer d.deinit(std.testing.allocator);
-
-    try std.testing.expectEqual(@as(u32, 16), d.digits);
-    try std.testing.expectEqual(@as(u32, 9), d.fractional);
-
-    try std.testing.expectEqual(@as(u8, '_'), ri.groupSeparator);
-    try std.testing.expectEqual(@as(u8, '.'), ri.decimalSeparator);
-    try std.testing.expectEqual(false, ri.indianNumbering);
+test "errors on thousand-marks after decimal point" {
+    const d = Self.init(std.testing.allocator, "3_141_592.650_123_430", null);
+    try std.testing.expectError(Error.IncorrectThousandMark, d);
 }
 
-test "parses indian notation" {
+test "error on incorrect thousand-mark placement" {
     // std.testing.log_level = .debug;
 
-    var ri = RenderingInformation{};
-    const d = try Self.init(std.testing.allocator, "3,14,15,926.501123", &ri);
-    defer d.deinit(std.testing.allocator);
+    const a = Self.init(std.testing.allocator, "3,14,1592.501", null);
+    try std.testing.expectError(Error.IncorrectThousandMark, a);
 
-    try std.testing.expectEqual(@as(u32, 14), d.digits);
-    try std.testing.expectEqual(@as(u32, 6), d.fractional);
+    const b = Self.init(std.testing.allocator, "31415,92.501", null);
+    try std.testing.expectError(Error.IncorrectThousandMark, b);
 
-    try std.testing.expectEqual(@as(u8, ','), ri.groupSeparator);
-    try std.testing.expectEqual(@as(u8, '.'), ri.decimalSeparator);
-    try std.testing.expectEqual(true, ri.indianNumbering);
+    const c = Self.init(std.testing.allocator, "31415_92.501", null);
+    try std.testing.expectError(Error.IncorrectThousandMark, c);
 }
 
-test "parses random notation" {
-    // std.testing.log_level = .debug;
-
-    var ri = RenderingInformation{};
-    const d = try Self.init(std.testing.allocator, "3,14,15,92,6.501", &ri);
-    defer d.deinit(std.testing.allocator);
-
-    try std.testing.expectEqual(@as(u32, 11), d.digits);
-    try std.testing.expectEqual(@as(u32, 3), d.fractional);
-
-    try std.testing.expectEqual(@as(u8, ','), ri.groupSeparator);
-    try std.testing.expectEqual(@as(u8, '.'), ri.decimalSeparator);
-    try std.testing.expectEqual(false, ri.indianNumbering);
+test "error on mixed thousand-marks" {
+    const d = Self.init(std.testing.allocator, "3_141,592.01", null);
+    try std.testing.expectError(Error.UnexpectedCharacter, d);
 }
 
 test "expand: no change" {
@@ -552,7 +414,7 @@ test "expand: only additional integers" {
     try std.testing.expectEqual(@as(u32, 2), spareChars);
 }
 
-test "expands: only additional fractional" {
+test "expand: only additional fractional" {
     // std.testing.log_level = .debug;
 
     var s = "0031,415.92".*; // dereference the pointer to the array
@@ -569,7 +431,7 @@ test "expands: only additional fractional" {
     try std.testing.expectEqual(@as(u32, 3), spareChars);
 }
 
-test "expands: additional integers and additional fractional" {
+test "expand: additional integers and additional fractional" {
     // std.testing.log_level = .debug;
 
     var s = "0031,415.92".*; // dereference the pointer to the array
